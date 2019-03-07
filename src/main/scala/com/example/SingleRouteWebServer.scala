2018b9adc0ba783bed61301c224569548120767a
takato.horikoshi <takato.h0rikosh1@gmail.com>
2019-03-07 22:33:45 +0900

    wip

--- src/main/scala/com/example/SingleRouteWebServer.scala
+++ src/main/scala/com/example/SingleRouteWebServer.scala
@@ -5,15 +5,17 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+
+import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 
-object WebServer {
+object SingleRouteWebServer {
   def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("my-system")
-    implicit val materializer = ActorMaterializer()
+    implicit val system: ActorSystem = ActorSystem("my-system")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
     // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
     val route =
       path("hello") {
